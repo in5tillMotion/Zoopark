002859534be89299fc194a5cf81898b4a6e9a4b3
in5tillMotion <waifusonmymind@gmail.com>
2020-07-19 18:26:29 +0400

    add_enum_in_Food_class

--- src/main/java/Food/Food.java
+++ src/main/java/Food/Food.java
@@ -1,11 +1,25 @@
 package Food;
 
 //Супер класс еды
-public abstract class Food {
-    protected String name;
+public enum Food {
+    BULL("Говядина", false),
+    PORK("Свинина", false),
+    GRASS("Трава", false),
+    WHEAT("Пшено", false);
 
-    @Override
-    public String toString() {
-        return name;
+    private final String title;
+    private final boolean isVegetable;
+
+    Food(String title, boolean isVegetable) {
+        this.title = title;
+        this.isVegetable = isVegetable;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isVegetable() {
+        return isVegetable;
     }
 }
